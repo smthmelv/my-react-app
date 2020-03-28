06583ffd7613f594685df8a6c82c9c43ccd25013
Brandon Slinkard <slinkardbrandon@gmail.com>
2020-03-28 00:39:01 -0500

    Update app.jsx to use react hooks
    A simplified example

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -6,6 +6,35 @@ import {connect} from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { getTypes } from '../../server/requests.js';
 
+// Here is the App component created using the
+// more modern/functional "hooks" api rather
+// than the class based structure using the class
+// methods like "render()" and "componentDidMount()"
+export const App = (props) => {
+  const [data, setData] = React.useState();
+  
+  // useEffect is a react hook that triggers any time the data
+  // inside the dependency array changes (the second parameter of this function)
+  // with an empty array passed (like we're doing) it just means this gets called once
+  // on the initial component render, then never again.
+  React.useEffect(() => {
+    // do some api call then call setData()
+    // something like the below example but it's
+    // just pseudocode so idk if it works as is or not
+    // axios.get(...).then(data => setData(data));
+  },[]);
+
+  // <></> is a new(ish) shorthand for <React.Fragment></React.Fragment>
+  return (
+   <>
+     <div className="App_header">
+       <h1>Hello World</h1>
+     </div>
+     <Score />
+   </>
+ );
+};
+
 class App extends React.Component {
     componentDidMount() {
         getTypes();
