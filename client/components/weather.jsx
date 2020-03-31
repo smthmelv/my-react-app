b00ce66ea990b453568f4ed9ff8c222cdeb4e607
smthmelv <smthmelv@iastate.edu>
2020-03-30 20:43:46 -0500

    completed input event

--- client/components/weather.jsx
+++ client/components/weather.jsx
@@ -1,21 +1,21 @@
 import Axios from 'axios';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
 import 'react-open-weather/lib/css/ReactWeather.css';
 import './weather.css';
 
 export const Weather = (props) => {
     const [weather, setWeather] = useState();
-    // const [city, setCity] = useState("London,UK");
-    const [city, setCity] = useState("Omaha,US");
+    const defaultCity = "Omaha,US";
+    const [city, setCity] = useState(defaultCity);
   
-    React.useEffect(() => {
+    useEffect(() => {
       Axios
-      .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=5f24adf9b82f6919270d8d3e8abefcc3&units=imperial&mode=html`)
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${city?city:defaultCity}&appid=5f24adf9b82f6919270d8d3e8abefcc3&units=imperial&mode=html`)
       .then(data => {
         setWeather(data.data)
       });
-    },[]);
+    },[city]);
   
     // <></> is a new(ish) shorthand for <React.Fragment></React.Fragment>
     return (
@@ -23,8 +23,9 @@ export const Weather = (props) => {
         <Container>
           <Row>
             <Col>
-            <h3 className="App_weather_txt">Input City</h3>
-              <input placeholder="Ex: Iowa City,US"></input>
+              <h3 className="App_weather_txt">Input City</h3>
+              <input placeholder="Ex: Iowa City,US" onBlur={e => setCity(e.target.value)}></input>
+              <div className="App_weather_txt">change happens on input blur.</div>
             </Col>
             <Col>
               <h3 className="App_weather_txt">Today's Weather</h3>
