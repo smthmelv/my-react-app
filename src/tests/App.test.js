2fd28b1c4bf57ea7878665515ec9a67c298b6a1d
smthmelv <smthmelv@iastate.edu>
2020-04-09 14:51:37 -0500

    new branch with some folder re-organizing

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Score from './Score';
+import App from '../components/App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Score />, div);
+  ReactDOM.render(<App />, div);
 });
--- src/tests/Score.test.js
+++ src/tests/Score.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Score from '../components/Score';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Score />, div);
 });
